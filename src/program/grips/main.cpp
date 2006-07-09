61b44a1ea687ff1c1f2eaedb429fd9aa21ee1091
nigels <nigels>
2006-07-09 15:11:54 +0000

    640x480 initial window size

--- src/program/grips/main.cpp
+++ src/program/grips/main.cpp
@@ -27,7 +27,7 @@ bool GlutMain(const std::vector<std::string> &arg)
     glutInit(&argc,argv);
 #endif
 
-    static Grips *window = new Grips(480,480,20,20);
+    static Grips *window = new Grips(640,480,20,20);
 
     if (arg.size()>1)
     {
