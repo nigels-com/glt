56cd521890788f6711e1734abeb3f7cc57c4f94e
nigels <nigels>
2007-05-07 03:30:18 +0000

    Remove revision logs

--- src/math/hash.cpp
+++ src/math/hash.cpp
@@ -1,15 +1,7 @@
 #include "hash.h"
 
+/* $Id: hash.cpp,v 2.2 2007/05/07 03:30:18 nigels Exp $ */
+
 /*! \file
     \ingroup Math
-
-    $Id: hash.cpp,v 2.1 2004/02/16 02:45:58 nigels Exp $
-
-    $Log: hash.cpp,v $
-    Revision 2.1  2004/02/16 02:45:58  nigels
-    Whitespace differences
-
-    Revision 1.1  2003/03/06 12:26:08  nigels
-    *** empty log message ***
-
 */
