73a2286d141522c9df1abbe9efc9db8ddbff4e76
nigels <nigels>
2007-05-10 03:01:30 +0000

    Add URL for GNU unifont

--- src/unifont/update.sh
+++ src/unifont/update.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# http://czyborra.com/unifont/
+
 printf "#ifndef GLT_UNIFONT_H\n#define GLT_UNIFONT_H\n\n" >  unifont.h
 printf "#include <unifont/config.h>\n\n"                  >> unifont.h
 printf "/* Compressed GNU Unifont */\n\n"                 >  unifont.cpp
 printf "extern unsigned char unicodeFont[];\n"            >> unifont.h
 printf "unsigned char %s[] = \n" unicodeFont              >> unifont.cpp 
-bin2src -z -ufont unifont.dat                             >> unifont.cpp
+bin2src -z -ufont unifont.hex                             >> unifont.cpp
 printf "\n#endif\n"                                       >> unifont.h
