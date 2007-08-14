a58d7f2a40574da021c86b48726738cc3a0cff14
nigels <nigels>
2007-08-14 04:16:33 +0000

    Link libraries now controlled by build system.

--- src/GL/freeglut_std.h
+++ src/GL/freeglut_std.h
@@ -55,11 +55,6 @@
 #    define FGAPI
 #    define FGAPIENTRY
 
-        /* Link with Win32 static freeglut lib */
-#       if defined(_MSC_VER)
-#           pragma comment (lib, "freeglut_static.lib")
-#       endif
-
 /* Windows shared library (DLL) */
 #   else
 
