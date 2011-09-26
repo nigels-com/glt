5c7826a03327e0d791c04add845468582647a314
Nigel Stewart <nigels@sourceforge.net>
2011-09-25 22:16:24 -0500

    Extend perlin.cpp front-end to support perlin, fbm, turbulence modes, supersampling

--- src/program/experimental/perlin.cpp
+++ src/program/experimental/perlin.cpp
@@ -1,6 +1,6 @@
 /*
   GLT OpenGL C++ Toolkit (LGPL)
-  Copyright (C) 2000-2007 Nigel Stewart
+  Copyright (C) 2000-2011 Nigel Stewart
 
   WWW:    http://www.nigels.com/glt/
   Forums: http://sourceforge.net/forum/?group_id=36869
@@ -21,8 +21,6 @@
 
 */
 
-/* $Id: perlin.cpp,v 1.2 2011/04/19 05:14:15 nigels Exp $ */
-
 /*! \file
     \brief   Utility for producing tilable greyscale perlin noise
     \ingroup Util
@@ -38,8 +36,11 @@ using namespace std;
 #include <misc/string.h>
 #include <math/noise.h>
 
-uint32 width          = 1024;
-uint32 fractionalBits = 3;
+uint32                    width          = 1024;
+uint32                    subSample      = 1;
+uint32                    fractionalBits = 27;
+GltNoisePerlin::NoiseType type           = GltNoisePerlin::NOISE_PERLIN;
+uint32                    randomSeed     = 0;
 
 std::string outputFilename;
 
@@ -47,37 +48,67 @@ const char *banner =
     "\n"                                           
     "perlin\n"
     "\n"                                           
-    "Tileable Perlin Noise Texture Utility      \n"
-    "GLT C++ OpenGL Toolkit                     \n"
-    "http://www.nigels.com/glt/                 \n"
+    "Tileable Perlin Noise Texture Utility                \n"
+    "GLT C++ OpenGL Toolkit                               \n"
+    "http://www.nigels.com/glt/                           \n"
     "\n"                                           
-    "Usage: bin2src [OPTION]                    \n"
+    "Usage: perlin [OPTION]                               \n"
     "\n"                                           
-    "  -h       Help.                           \n"
-    "  -w int   Texture width.   (default=1024) \n"
-    "  -b int   Fractional bits. (default=25)   \n"
-    "  -o file  Output to file instead of cout. \n";
+    "  -h       Help.                                     \n"
+    "  -w int   Texture width.           (default=1024)   \n"
+    "  -s int   i^2 subsampling.         (default=1)      \n"
+    "  -b int   Fractional bits.         (default=27)     \n"
+    "  -t name  perlin, fbm, turbulence. (default=perlin) \n"
+    "  -r int   random integer seed.     (default=0)      \n"
+    "  -o file  Output to file instead of cout.           \n";
 
 bool 
-tiledNoiseImage(std::string &image,const uint32 width,const uint32 height,const uint32 fractionalBits)
+tiledNoiseImage
+(
+    std::string                    &image,
+    const uint32                    width,
+    const uint32                    height,
+    const uint32                    subSample,
+    const uint32                    fractionalBit,
+    const GltNoisePerlin::NoiseType type,
+    const uint32                    randomSeed
+)
 {
     const uint32 channels = image.size()/(width*height);
     assert(channels*width*height==image.size());
 
     byte *i = reinterpret_cast<byte *>(&image[0]);
 
-    // Hard-coded to 1024 width/height - revisit
+    const uint32 ss     = subSample-1;
+    const uint32 xMax   = width<<ss;
+    const uint32 yMax   = height<<ss;
+    const uint32 step   = 1<<ss;
+    const float  step2i = real(1.0)/real(step*step); 
+    const uint32 shift  = 32-log2(xMax);
 
-    const uint32 leftShift  = 22;
-    const uint32 fractional = 22+fractionalBits;
+    GltNoisePerlin noise(randomSeed);
+    noise.type() = type;
+    noise.octaves() = 3;
 
-    GltNoisePerlin noise;
-
-    for (uint32 y=0; y<height; y++)
-        for (uint32 x=0; x<width; x++)
+    for (uint32 y=0; y<yMax; y += step)
+        for (uint32 x=0; x<xMax; x += step)
         {
-            real v = noise.val(x<<leftShift,y<<leftShift,fractional);
-            byte b = (byte) floor((v+1)*0.5*255.0);
+            real v = 0.0;
+            for (uint32 sy=0; sy<step; ++sy)
+                for (uint32 sx=0; sx<step; ++sx)
+                {
+                  v += noise.val
+                    (
+                      (x+sx)<<shift,
+                      (y+sy)<<shift,
+                      fractionalBit
+                    );
+                }
+
+            const byte b = 
+                noise.type()==GltNoisePerlin::NOISE_TURBULENCE ?
+                    (byte) clamp((v*step2i)*255.0,0.0,255.0) :
+                    (byte) clamp((v*step2i+1)*0.5*255.0,0.0,255.0);
 
             for (uint32 c=0; c<channels; c++)
                 *(i++) = b;
@@ -97,7 +128,19 @@ main(int argc,char *argv[])
 
         if (arg=="-o")      { i++; if (i<argc) outputFilename = argv[i];       continue; }
         if (arg=="-w")      { i++; if (i<argc) width = atoi(argv[i]);          continue; }
+        if (arg=="-s")      { i++; if (i<argc) subSample = atoi(argv[i]);      continue; }
         if (arg=="-b")      { i++; if (i<argc) fractionalBits = atoi(argv[i]); continue; }
+        if (arg=="-t")
+        { 
+            i++;
+            if (i<argc)
+            {
+                if (!strcmp(argv[i],"perlin")) { type = GltNoisePerlin::NOISE_PERLIN; continue; }
+                if (!strcmp(argv[i],"fbm")) { type = GltNoisePerlin::NOISE_FBM; continue; }
+                if (!strcmp(argv[i],"turbulence")) { type = GltNoisePerlin::NOISE_TURBULENCE; continue; }
+            }
+        }
+        if (arg=="-r")      { i++; if (i<argc) randomSeed = atoi(argv[i]); continue; }
 
         cout << banner;
         cout << endl;
@@ -109,7 +152,10 @@ main(int argc,char *argv[])
 
     std::string image;
     image.resize(width*width);
-    tiledNoiseImage(image,width,width,fractionalBits);
+    
+    fractionalBits = clamp(fractionalBits,0u,31u);
+
+    tiledNoiseImage(image,width,width,subSample,fractionalBits,type,randomSeed);
 
     std::string data;
     #ifdef GLT_PNG
